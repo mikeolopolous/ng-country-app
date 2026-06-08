de966c6c327f119bfbaca762af23de7b691a8cfe
Miguel Lopez <ing.miguellv@gmail.com>
2026-06-08 12:37:14 -0600

    add NotFound component and implement error handling in CountryPage

--- src/app/country/pages/country-page/country-page.ts
+++ src/app/country/pages/country-page/country-page.ts
@@ -1,15 +1,28 @@
 import { Component, inject } from '@angular/core';
-import { toSignal } from '@angular/core/rxjs-interop';
 import { ActivatedRoute } from '@angular/router';
-import { map } from 'rxjs';
+import { rxResource } from '@angular/core/rxjs-interop';
+
+import { CountryService } from './../../services/country';
+import { NotFound } from '../../../shared/components/not-found/not-found';
+import { CountryInformation } from './country-information/country-information';
 
 @Component({
   selector: 'app-country-page',
-  imports: [],
+  imports: [NotFound, CountryInformation],
   templateUrl: './country-page.html',
 })
 export class CountryPage {
-  countryCode = toSignal(
-    inject(ActivatedRoute).params.pipe(map((params) => params['countryCode'])),
-  );
+  // countryCode = toSignal<string>(
+  //   inject(ActivatedRoute).params.pipe(map((params) => params['country-code'])),
+  // );
+
+  CountryService = inject(CountryService);
+  countryCode = inject(ActivatedRoute).snapshot.params['country-code'];
+
+  countryResource = rxResource({
+    params: () => ({ code: this.countryCode }),
+    stream: ({ params }) => {
+      return this.CountryService.searchCountryByAlphaCode(params.code);
+    },
+  });
 }
